086bb44ee08263266688d5c74dee0fce28949f64
IvanaSaraber <irsaraber@gmail.com>
2025-04-24 14:55:17 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welkom bij GazaAid!</h1>
+      <p>Dit is de homepagina van je Next.js app.</p>
+    </div>
+  );
+}
