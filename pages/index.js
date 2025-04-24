a39cd12d7d2f2e55b88a94b7125ba9a437682241
IvanaSaraber <irsaraber@gmail.com>
2025-04-24 20:43:41 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [campaigns, setCampaigns] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/campaigns')
+      .then((res) => res.json())
+      .then((data) => setCampaigns(data.records || []));
+  }, []);
+
   return (
     <div>
-      <h1>Welkom bij GazaAid!</h1>
-      <p>Dit is de homepagina van je Next.js app.</p>
+      <h1>GazaAid Campagnes</h1>
+      {campaigns.length === 0 ? (
+        <p>Laden...</p>
+      ) : (
+        <ul>
+          {campaigns.map((item) => (
+            <li key={item.id}>
+              <strong>{item.fields.Titel}</strong><br />
+              €{item.fields.Gedoneerd} van €{item.fields.Doelbedrag}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
