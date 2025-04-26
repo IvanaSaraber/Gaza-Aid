caef6516b1e7ef6f049273be3732f393a7e7a9ef
IvanaSaraber <irsaraber@gmail.com>
2025-04-26 19:42:31 +0200

    Update campaigns.js

--- pages/api/campaigns.js
+++ pages/api/campaigns.js
@@ -1,6 +1,13 @@
 // pages/api/campaigns.js
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method !== 'GET') {
+    return res.status(405).end(); // Method Not Allowed
+  }
   const apiKey = process.env.AIRTABLE_API_KEY
   const baseId = process.env.AIRTABLE_BASE_ID
   const tableName = 'MainTable' // pas aan als je tabel anders heet
